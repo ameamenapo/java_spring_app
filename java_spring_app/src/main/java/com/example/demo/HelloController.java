71e5ad3a4c2e23f5b3fce1af8f6196bfbf95a198
TANIBUCHIHIROKAZU <tanibuchi1121@gmail.com>
2024-03-15 01:43:02 +0900

    リセット

--- java_spring_app/src/main/java/com/example/demo/HelloController.java
+++ java_spring_app/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 	
-	@RequestMapping(value="/index")
+	@GetMapping("/index")
 	private String index() {
-		return "index.html";
+		return "index";
+	}
+	
+	@GetMapping("/test")
+	private String test(Model model) {
+		model.addAttribute("message", "messageをmodelに設定");
+		model.addAttribute("modelValue", "modelvalue");
+		return "test";
 	}
 
 }
